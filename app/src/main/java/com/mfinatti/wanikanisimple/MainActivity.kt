c90d8c5aa1803357b79cbd909fd1904a5bab94cc
Matheus Finatti <mf.finatti@gmail.com>
2024-07-13 09:58:01 +0100

    feat: create login screen

--- app/src/main/java/com/mfinatti/wanikanisimple/MainActivity.kt
+++ app/src/main/java/com/mfinatti/wanikanisimple/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mfinatti.wanikanisimple.login.LoginScreen
 import com.mfinatti.wanikanisimple.ui.theme.WaniKaniSimpleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,8 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WaniKaniSimpleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    LoginScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
